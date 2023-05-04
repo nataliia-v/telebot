65c07d680c19acd47b4323fef88d28c1f41c37e7
nataliia-v <nataly.verbenskaya@gmail.com>
2023-05-05 00:09:55 +0300

    added telebot.go

--- cmd/telebot.go
+++ cmd/telebot.go
@@ -10,9 +10,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// kbotCmd represents the kbot command
-var kbotCmd = &cobra.Command{
-	Use:   "kbot",
+// telebotCmd represents the telebot command
+var telebotCmd = &cobra.Command{
+	Use:   "telebot",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -21,20 +21,20 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("kbot called")
+		fmt.Println("telebot called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(kbotCmd)
+	rootCmd.AddCommand(telebotCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// kbotCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// telebotCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// kbotCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// telebotCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
